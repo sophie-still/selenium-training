a45c982c91ef0f3c8903960047168f072ea9393c
stephen.kam <stephenk@outlook.com>
2018-10-19 16:32:17 +0100

    fixed initialisation error

--- src/test/scala/seleniumtests/utils/Driver.scala
+++ src/test/scala/seleniumtests/utils/Driver.scala
@@ -3,5 +3,5 @@ package seleniumtests.utils
 import org.openqa.selenium.WebDriver
 
 trait Driver {
-implicit val driver: WebDriver = DriverInstance.getInstance().get
+  implicit val driver: WebDriver = DriverInstance.getInstance().get
 }
--- src/test/scala/seleniumtests/utils/DriverInstance.scala
+++ src/test/scala/seleniumtests/utils/DriverInstance.scala
@@ -6,23 +6,25 @@ import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.ie.InternetExplorerDriver
 import org.openqa.selenium.safari.SafariDriver
 
-object DriverInstance extends Driver {
-  var currentDriver:Option[WebDriver] = None
+object DriverInstance {
 
-  def getInstance():Option[WebDriver] = {
+  var currentDriver: Option[WebDriver] = None
+
+  def getInstance(): Option[WebDriver] = {
     if (currentDriver.isEmpty) {
       val driver = createInstance()
       currentDriver = Some(driver)
     }
     currentDriver
   }
-def createInstance():WebDriver = {
-  val browserType = Option(System.getProperty("browser")).getOrElse("chrome")
-  browserType match {
-    case "chrome" ⇒ new ChromeDriver()
-    case "firefox" ⇒ new FirefoxDriver()
-    case "explorer" ⇒ new InternetExplorerDriver()
-    case "safari" ⇒ new SafariDriver()
+
+  def createInstance(): WebDriver = {
+    val browserType = Option(System.getProperty("browser")).getOrElse("chrome")
+    browserType match {
+      case "chrome" ⇒ new ChromeDriver()
+      case "firefox" ⇒ new FirefoxDriver()
+      case "explorer" ⇒ new InternetExplorerDriver()
+      case "safari" ⇒ new SafariDriver()
+    }
   }
-}
 }
